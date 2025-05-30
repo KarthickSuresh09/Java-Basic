befc391b3a7e42978a6e70ef6b801ed1e4e96da6
karthick suresh <105416723+KarthickSuresh09@users.noreply.github.com>
2025-05-30 17:03:17 +0530

    Add files via upload

---
+++ doubleconvertion.java
@@ -0,0 +1,11 @@
+package Com.Package.Day3;
+
+public class doubleconvertion {
+	public static void main (String[]args)
+	{
+		double a= Double.parseDouble(args[0]);
+		System.out.println(a+12);
+		System.out.println(args[0]+12);
+	}
+
+}
